a52a9e447f435c34f8aa6baceb70357f23e24fce
Vincent Bremer <vbremer89@gmail.com>
2023-03-26 10:22:43 +0200

    adding todos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Todo;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $todos = Todo::latest()->get();
+
+    return view('todos', compact('todos'));
 });
 
-Route::post('todo', function(Request $request) {
-    return $request->input('description');
+Route::post('todos/new', function(Request $request) {
+    $request->validate([
+        'description' => 'required'
+    ]);
+
+    Todo::create([
+        'description' => $request->input('description')
+    ]);
+
+    return back();
+});
+
+Route::post('todos/delete/{todo}', function(Request $request, Todo $todo) {
+    $todo->delete();
+
+    return back();
 });
